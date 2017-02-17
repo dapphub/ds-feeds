0a4d5cc9e79190d75e7f96fad1b8b3810a699d4f
Gonzalo Balabasquer <gbalabasquer@gmail.com>
2017-02-17 14:32:31 -0300

    npm run build for js_module

---
+++ cli/gulpfile.js
@@ -0,0 +1,24 @@
+var exec = require('child_process').exec
+var gulp = require('gulp')
+
+// npm run build
+gulp.task('dapple-build', function (cb) {
+  exec('dapple build', {cwd: '../'}, function (err, res, failed) {
+    if (err) {
+      console.log(err)
+    } else if (failed) {
+      process.stdout.write(failed)
+    } else {
+      process.stdout.write('\u001b[32mDapple build completed!\n')
+    }
+    cb(err)
+  })
+})
+
+
+gulp.task('build', ['dapple-build'], function (){
+  return gulp.src([
+      '../build/js_module.js'
+  ])
+  .pipe(gulp.dest('lib/'))
+})
